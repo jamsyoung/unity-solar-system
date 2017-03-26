b8d21c212a0b4c57c83d5ae73d9a639ad98b8b45
James Young <jmeyoung@gmail.com>
2017-03-25 23:45:05 -0400

    initial commit

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public GameObject planet;
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(planet.transform.position, Vector3.up, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {	
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, speed, 0) * Time.deltaTime);
+	}
+}
